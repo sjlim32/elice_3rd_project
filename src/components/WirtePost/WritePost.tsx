2ea00a6bebe63da600a90d9e9efbb7d78dda0767
Y.S <103623717+lili-ys@users.noreply.github.com>
2023-04-25 20:22:04 +0900

    feat: new post

--- src/components/WirtePost/WritePost.tsx
+++ src/components/WirtePost/WritePost.tsx
@@ -1,77 +1,65 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import {
   WritePostContainer,
   WriteFormContainer,
   SubmitButtonContainer,
   InputsContainer,
   SummaryWrapper,
-} from './write-post-styled'
-import { useRouter } from 'next/router'
+} from './write-post-styled';
+import { useRouter } from 'next/router';
 
 interface Props {
-  isEdit: Boolean
+  isEdit: Boolean;
 }
 
 const WritePost = ({ isEdit }: Props) => {
-  const [writeInputs, setWriteInputs] = useState({
-    title: '',
-    content: '',
-    summary: '',
-  })
+  const titleRef = useRef<HTMLInputElement>(null);
+  const contentRef = useRef<HTMLInputElement>(null);
+  const summaryRef = useRef<HTMLInputElement>(null);
 
-  const router = useRouter()
-  const { title, content, summary } = writeInputs
+  const router = useRouter();
 
   useEffect(() => {
-    console.log('edit', isEdit)
-    if (isEdit) {
-      setWriteInputs({
-        title: 'hi',
-        content: 'hihi',
-        summary: 'ddd',
-      })
+    console.log('edit', isEdit);
+    if (router.isReady && isEdit) {
+      const { id } = router.query;
+      console.log('id', id);
+      titleRef.current.value = '수정 타이틀' + id;
+      contentRef.current.value = '수정 내용' + id;
+      summaryRef.current.value = '수정 요약' + id;
     }
-  }, [isEdit])
+  }, [router]);
 
-  useEffect(() => {
-    const { id } = router.query
-    console.log('id', id)
-  }, [router])
-
-  const onChangeInputs = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value, name } = e.target
-    setWriteInputs({
-      ...writeInputs,
-      [name]: value,
-    })
-  }
-
-  const resetInputs = () => {
-    setWriteInputs({
-      title: '',
-      content: '',
-      summary: '',
-    })
-  }
+  // useEffect(() => {
+  //   if (router.isReady) {
+  //     const { id } = router.query;
+  //     console.log('id', id);
+  //   }
+  // }, [router]);
 
   const handleSubmit = () => {
-    const { title, content, summary } = writeInputs
-    console.log('저장', title, content, summary)
-    resetInputs()
-  }
+    if (!titleRef.current?.value) {
+      return titleRef.current?.focus();
+    }
+
+    if (!contentRef.current?.value) {
+      return contentRef.current?.focus();
+    }
+
+    const title = titleRef.current?.value;
+    const content = contentRef.current?.value;
+    const summary = summaryRef.current?.value;
+    console.log('저장', title, content, summary);
+  };
 
   const handleDelete = () => {
-    console.log('삭제')
-    resetInputs()
-  }
+    console.log('삭제');
+  };
 
   const getSummary = () => {
-    const summary = '요약'
-    setWriteInputs({
-      ...writeInputs,
-      summary,
-    })
-  }
+    const summary = '요약';
+    if (summaryRef.current) summaryRef.current.value = summary;
+  };
 
   return (
     <WritePostContainer>
@@ -84,32 +72,23 @@ const WritePost = ({ isEdit }: Props) => {
           <input
             name="title"
             placeholder="제목을 입력하세요"
-            value={title}
-            onChange={onChangeInputs}
             type="text"
-            required
+            ref={titleRef}
           />
           <input
             name="content"
             placeholder="내용을 입력하세요"
-            value={content}
-            onChange={onChangeInputs}
             type="text"
-            required
+            ref={contentRef}
           />
           <SummaryWrapper>
             <span onClick={getSummary}>요약하기</span>
-            <input
-              name="summary"
-              value={summary}
-              onChange={onChangeInputs}
-              type="text"
-            />
+            <input name="summary" type="text" ref={summaryRef} />
           </SummaryWrapper>
         </InputsContainer>
       </WriteFormContainer>
     </WritePostContainer>
-  )
-}
+  );
+};
 
-export default WritePost
+export default WritePost;
