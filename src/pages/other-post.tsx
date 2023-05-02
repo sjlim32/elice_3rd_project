7e334bc04d8f94fb68bbfcfd7066ffac20f6a73f
sjlim32 <32sjlim@gmail.com>
2023-05-02 17:58:38 +0900

    feat: 회원정보 수정페이지 구현, hook 폴더 수정

---
+++ src/pages/other-post.tsx
@@ -0,0 +1,12 @@
+import UserPost from '../components/OtherUser/OtherPost';
+
+const otherPost = () => {
+	return (
+		<>
+			<div>OtherPostPage</div>
+			<UserPost />
+		</>
+	);
+};
+
+export default otherPost;
