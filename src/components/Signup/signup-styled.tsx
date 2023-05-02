ab61c73702074e09fadb17d302289f642996b3fc
Yeonjun Jung <duswns12893@gmail.com>
2023-05-03 03:06:10 +0900

    fix: 파이어베이스 invaild auth/api-key 문제 해결 중

--- src/components/Signup/signup-styled.tsx
+++ src/components/Signup/signup-styled.tsx
@@ -1,4 +1,8 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import Login from '../Login/Login';
+import { auth } from '../common/fbase-config';
 
 
 const Container = styled.div`
@@ -90,10 +94,31 @@ const Bio = styled.textarea`
 `
 
 export const SignupForm = () => {
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    // 새로운 유저인지 확인(초기값: true)
+
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        // 회원가입 로직 구현        
-};
+        // 회원가입 로직 구현
+    };
+
+    const Change = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        event.preventDefault();
+        try{
+            let data = await createUserWithEmailAndPassword(
+                auth, email, password);
+            alert("회원가입 성공");
+            Login();
+            console.log(data);
+        }
+        catch(error){
+            console.log(error);
+            // 이메일 중복 검사
+            alert("해당 이메일은 이미 사용중 이거나 또는 사용할 수 없는 이메일 입니다.");            
+        }
+    }
 
     return (
         <Container>
@@ -105,9 +130,13 @@ export const SignupForm = () => {
                     <InputTitle>이름</InputTitle>
                     <Input type="text" placeholder="name" required />                    
                     <InputTitle>이메일</InputTitle>
-                    <Input type="email" id='email' placeholder="example@email.com" required />
+                    <Input type="email" id='email' placeholder="example@email.com" required defaultValue={email || ""} onChange={e => {
+                        setEmail(e.target.value);
+                    }}/>
                     <InputTitle>비밀번호</InputTitle>
-                    <Input type="password" className='password' placeholder="●●●●●●●" required />
+                    <Input type="password" className='password' placeholder="●●●●●●●" required defaultValue={password || ""} onChange={e => {
+                        setPassword(e.target.value);
+                    }}/>
                     <InputTitle>블로그명</InputTitle>
                     <Input type="text" placeholder='최소 4자~최대 32자의 영문 소문자,숫자,하이픈 포함' required/>
                     <InputTitle>블로그 소개</InputTitle>
