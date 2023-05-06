13737522a74d46e5f7927353f6b850813befc850
YKS <ykson94@gmail.com>
2023-05-06 11:27:21 +0900

    feat: toast-ui

--- src/components/WirtePost/ToastTest.tsx
+++ src/components/WirtePost/ToastTest.tsx
@@ -1,16 +1,18 @@
 import dynamic from 'next/dynamic';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { ForwardedRef, forwardRef, useEffect, useRef, useState } from 'react';
-import { Editor } from '@toast-ui/react-editor';
+import { Editor, EditorProps } from '@toast-ui/react-editor';
 // import axios from 'axios';
 
 const EditorDynamic = dynamic(() => import('./Editor'), {
   ssr: false,
 });
 
-const EditorForwardRef = forwardRef((props, ref: ForwardedRef<Editor>) => {
-  return <EditorDynamic {...props} forwardedRef={ref} />;
-});
+const EditorForwardRef = forwardRef(
+  (props: EditorProps, ref: ForwardedRef<Editor>) => {
+    return <EditorDynamic {...props} forwardedRef={ref} />;
+  }
+);
 
 EditorForwardRef.displayName = 'Editor';
 
@@ -26,29 +28,20 @@ export default function BoardWrite() {
   }, []);
 
   const onSubmit = () => {
-    // console.log('html', contentRef.current?.getInstance().getHTML());
-    // console.log('markdown', contentRef.current?.getInstance().getMarkdown());
-    // const testtest = contentRef.current?.getInstance().getMarkdown();
+    const innerHTMLText = contentRef.current?.getInstance().getHTML();
 
-    // const result = axios.post('http://127.0.0.1:3080/api/v1/posts', {
-    //   title: 'hi',
-    //   content: testtest,
-    // });
-
-    const innerHTMLText = contentRef.current?.getInstance().getHTML()
-
-    const tempDiv = document.createElement('div')
+    const tempDiv = document.createElement('div');
 
     tempDiv.innerHTML = innerHTMLText as string;
 
-    const text = tempDiv.textContent
+    const text = tempDiv.textContent;
 
-    console.log(text)
-    console.log('^^^ text')
+    console.log(text);
+    console.log('^^^ text');
   };
 
   return (
-    <div id="textContentTest" style={{ height: '400px' }}>
+    <div style={{ height: '400px' }}>
       <EditorForwardRef
         ref={contentRef}
         initialValue={value}
