f8e50b63c30c278ed6e669dfcf9cadb5c89fa30c
YKS <ykson94@gmail.com>
2023-05-09 18:25:57 +0900

    feat:toast ui editor

--- src/pages/write.tsx
+++ src/pages/write.tsx
@@ -1,11 +1,9 @@
 import WritePost from '@/components/WirtePost/WritePost';
-import ToastTest from '@/components/WirtePost/ToastTest';
 
 export default function WritePage() {
   return (
     <>
-      {/* <WritePost /> */}
-      <ToastTest />
+      <WritePost />
     </>
   );
 }
