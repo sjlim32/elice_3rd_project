a92b103e89de802e36efe24310da05d98273cc26
ys9494 <mdlife94@gmail.com>
2023-05-12 18:56:15 +0900

    feat:api

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,34 +1,34 @@
-import { MainWrapper, MainHeader, MainNav, MainNavImage } from './main-styled'
+import { MainWrapper, MainHeader, MainNav, MainNavImage } from './main-styled';
 
 const Main = () => {
-
   return (
     <MainWrapper>
       <MainHeader>
         <MainNavImage
-        className='trending'
-        src="/images/Trending.png"
-        alt="트렌딩"
-        width="25"
-        height="25"
+          className="trending"
+          src="/images/Trending.png"
+          alt="트렌딩"
+          width="25"
+          height="25"
         />
         <MainNav>Trending</MainNav>
         <MainNavImage
-        className='subscribe'
-        src="/images/Subscribe.png"
-        alt="구독"
-        width="23"
-        height="23"
+          className="subscribe"
+          src="/images/Subscribe.png"
+          alt="구독"
+          width="23"
+          height="23"
         />
         <MainNav>SubScribe</MainNav>
         <MainNavImage
-        className='recentPosts'
-        src="/images/RecentPosts.png"
-        alt="최신게시물"
-        width="23"
-        height="23"
+          className="recentPosts"
+          src="/images/RecentPosts.png"
+          alt="최신게시물"
+          width="23"
+          height="23"
         />
         <MainNav>Recent Posts</MainNav>
+        <div></div>
       </MainHeader>
     </MainWrapper>
   );
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,68 +1,9 @@
-// import axios, { AxiosResponse } from "axios";
-
-// // 현재 mock server api 주소임
-// const api = axios.create({
-//   baseURL: process.env.NEXT_PUBLIC_AXIOS_BASE_URL,
-//   // withCredentials: true,
-// });
-
-// interface Response<T> extends AxiosResponse {
-//   data: T;
-// }
-
-// async function get<T>(endpoint: string): Promise<Response<T>> {
-//   console.log(`GET 요청 : ${endpoint}`);
-
-//   try {
-//     const response = api.get(endpoint, {
-//       // headers: {
-//       //   Authorization: `Bearer ${localStorage.getItem("token")}`,
-//       // },
-//     })
-//     return response as Promise<Response<T>>;
-//   } catch (error) {
-//     console.error('GET 요청 ERROR', error)
-//     throw error
-//   }
-// }
-
-// async function post<T>(endpoint: string, data: unknown): Promise<Response<T>> {
-//   console.log(`POST 요청 : ${endpoint}`);
-
-//   return api.post(endpoint, data, {
-//     // headers: {
-//     //   "Content-Type": "application/json",
-//     //   Authorization: `Bearer ${localStorage.getItem("token")}`,
-//     // },
-//   }) as Promise<Response<T>>;
-// }
-
-// async function patch<T>(endpoint: string, data: unknown): Promise<Response<T>> {
-//   console.log(`PATCH 요청 : ${endpoint}`);
-
-//   return api.patch(endpoint, data, {
-//     // headers: {
-//     //   "Content-Type": "application/json",
-//     //   Authorization: `Bearer ${localStorage.getItem("token")}`,
-//     // },
-//   }) as Promise<Response<T>>;
-// }
-
-// async function del<T>(endpoint: string): Promise<Response<T>> {
-//   console.log(`DELETE 요청 : ${endpoint}`);
-
-//   return api.delete(endpoint, {
-//     // headers: {
-//     //   Authorization: `Bearer ${localStorage.getItem("token")}`,
-//     // },
-//   }) as Promise<Response<T>>;
-// }
-
-// export { api, get, post, patch, del as delete };
-
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosResponse } from 'axios';
+import auth from '@/components/common/auth';
 
 const baseURL = process.env.NEXT_PUBLIC_AXIOS_BASE_URL;
+
+const user = auth.currentUser;
 
 async function request<T>(
   method: string,
@@ -72,10 +13,20 @@ async function request<T>(
   console.log(`${method} 요청: ${endpoint}`);
 
   try {
+    let headers = {};
+
+    if (user) {
+      const idToken = await user.getIdToken();
+      headers = {
+        Authorization: `Bearer ${idToken}`,
+      };
+    }
+
     const response = await axios.request<T>({
       method,
       url: endpoint,
       baseURL,
+      headers,
       data,
     });
 
@@ -86,26 +37,20 @@ async function request<T>(
   }
 }
 
- function get<T>(endpoint: string): Promise<AxiosResponse<T>> {
-  return request<T>("GET", endpoint);
+function get<T>(endpoint: string): Promise<AxiosResponse<T>> {
+  return request<T>('GET', endpoint);
 }
 
- function post<T>(
-  endpoint: string,
-  data: unknown
-): Promise<AxiosResponse<T>> {
-  return request<T>("POST", endpoint, data);
+function post<T>(endpoint: string, data: unknown): Promise<AxiosResponse<T>> {
+  return request<T>('POST', endpoint, data);
 }
 
- function patch<T>(
-  endpoint: string,
-  data: unknown
-): Promise<AxiosResponse<T>> {
-  return request<T>("PATCH", endpoint, data);
+function patch<T>(endpoint: string, data: unknown): Promise<AxiosResponse<T>> {
+  return request<T>('PATCH', endpoint, data);
 }
 
- function del<T>(endpoint: string): Promise<AxiosResponse<T>> {
-  return request<T>("DELETE", endpoint);
+function del<T>(endpoint: string): Promise<AxiosResponse<T>> {
+  return request<T>('DELETE', endpoint);
 }
 
 export { get, post, patch, del as delete };
