2ac339d4360a70ae5bd7362f3195cd7cef701760
ys9494 <mdlife94@gmail.com>
2023-05-15 11:13:01 +0900

    feat: util

--- src/components/RecoilTest/RecoilTest.tsx
+++ src/components/RecoilTest/RecoilTest.tsx
@@ -1,6 +1,7 @@
 import { useRecoilState } from 'recoil';
 import { testState } from '@/store/test';
 import { ChangeEvent } from 'react';
+import { getUserInfo } from '@/utils/util';
 
 const RecoilTest = () => {
   const [test, setTest] = useRecoilState(testState);
@@ -9,11 +10,19 @@ const RecoilTest = () => {
     setTest(e.target.value);
   };
 
+  const getUser = () => {
+    const userInfo = getUserInfo();
+    console.log('user', userInfo);
+  };
+
   return (
     <>
       <div>test component</div>
       <input type="text" value={test} onChange={testRecoil} />
       <div>{test}</div>
+      <button type="button" onClick={getUser}>
+        유저정보 가져오기
+      </button>
     </>
   );
 };
