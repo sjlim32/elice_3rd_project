51d998186160ef22259f1854014a91edc33209f6
[AI6/1반] 손유경 <ykson94@gmail.com>
2023-05-16 15:10:44 +0900

    feat:get user

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,10 +1,39 @@
 import auth from '@/components/common/auth';
 
-export const getUserInfo = () => {
-  const user = auth.currentUser;
+// export const getUserInfo = async () => {
+//   return new Promise((resolve) => {
+//     let email = "";
+//     let nickname = "";
 
-  const email = user?.email;
-  const nickname = user?.displayName;
+//     auth.onAuthStateChanged((user) => {
+//       if (user) {
+//         email = user?.email;
+//         nickname = user?.displayName;
+//       }
 
-  return { email, nickname };
+//       resolve({ email, nickname });
+//     });
+//   });
+// };
+
+export const getUserInfo = async () => {
+  return new Promise((resolve) => {
+    const user = auth.currentUser;
+    if (user) {
+      resolve({
+        email: user.email || "",
+        nickname: user.displayName || "",
+      });
+    } else {
+      const unsubscribe = auth.onAuthStateChanged((user) => {
+        if (user) {
+          resolve({
+            email: user.email || "",
+            nickname: user.displayName || "",
+          });
+          unsubscribe(); // 사용자 정보가 얻어진 후에는 이벤트 리스너 해제
+        }
+      });
+    }
+  });
 };
