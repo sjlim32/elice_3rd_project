f65ec1de774576d04294fd17970110cd33cd954b
jingwangjjang <jingwangjjang@gmail.com>
2023-05-17 02:51:11 +0900

    feat: 댓글 post요청 가능 및 params 가져오기

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
-import * as api from '@/utils/api';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import auth from '@/components/common/auth';
+import { useRouter } from "next/router";
 
 const Title = styled.div`
   width: 40%;
@@ -43,12 +46,12 @@ const MorePostTitle = styled.div`
   color: black;
 `;
 
-const CommentInputDiv = styled.div`
+const CommentInputForm = styled.form`
   width: 40%;
   margin: 1em auto;
 `;
 
-const CommentInput = styled.textarea`
+const CommentInput = styled.textarea `
   width: 100%;
   height: 6em;
   background-color: #f1f3f5;
@@ -68,49 +71,95 @@ const Comment = styled.div`
   font-size: 1rem;
 `;
 
+interface CommentFormValue {
+  content: string
+}
+
 interface PostDetail {
-  id: number,
-  category: string,
-  title: string,
-  summary: string,
-  user: { nickname: string },
+  Category: string,
+  CategoryId: number,
+  Comments: 
+    { User: { nickname: string },
+    content: string,
+    createdAt: Date,
+    id: number }[]
+  ,
+  User: { nickname: string },
+  UserId: string,
   content: string,
-  views: number,
-  created_at: Date,
-  updated_at: Date
+  createdAt: Date,
+  deletedAt: Date,
+  id: number,
+  summary: string,
+  title: string,
+  updatedAt: Date,
+  userId: string,
+  views: number
 };
 
 export default function Posts() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CommentFormValue>();
+
+  const user = auth.currentUser || null;
+  const router = useRouter();
+  const [Id, setId] = useState<string>("")
   const [Postdata, setPostdata] = useState<PostDetail>({})
 
   useEffect(() => {
     getPost()
+    setId(String(router.query.id))
   }, []);
 
   const getPost = async () => {
-    const response = await api.get<PostDetail>('/posts/1')
-    setPostdata(response.data)
+    const response = await axios.get(`http://3.39.79.138/api/v1/posts/${Id}`);
+    console.log(response.data.data)
+    setPostdata(response.data.data)
+  };
+
+  const onCommentSubmitHandler: SubmitHandler<CommentFormValue> = async (data) => {
+    try {
+        await axios.post(`http://3.39.79.138/api/v1/comments/${Id}`, data);
+        // window.location.replace("/msg")
+    } catch (error : any) {
+        alert(error);
+    }
   };
 
   return (
     <>
       <Title>{Postdata.title}</Title>
-      <Author>{Postdata.user.nickname} · <span style={{color: "gray", fontSize: "0.7rem"}}>Date</span></Author>
+      <Author>{Postdata.User.nickname} · <span style={{color: "gray", fontSize: "0.7rem"}}>{new Date(Postdata.createdAt).toString().substring(0, 21)}</span></Author>
       <Post>{Postdata.content}</Post>
-      <MorePosts>
-        <MorePost>Prev Post <MorePostTitle>Prev title blah blah</MorePostTitle></MorePost>
-        <MorePost>Next Post <MorePostTitle>Next title blah blah</MorePostTitle></MorePost>
-      </MorePosts>
 
-      <CommentInputDiv>
-        <div style={{padding: "1em 0", fontSize: "0.7rem"}}>1 comments</div>
-        <CommentInput/>
-      </CommentInputDiv>
+      <CommentInputForm onSubmit={handleSubmit(onCommentSubmitHandler)}>
+        <div style={{padding: "1em 0", fontSize: "0.7rem"}}>{Postdata.Comments.length} comments</div>
+        <CommentInput {...register('content', {
+          required: true,
+          minLength: 1,
+          maxLength: 200,
+        })} />
+        <button>Send</button>
+      </CommentInputForm>
 
       <div>
-        <CommentAuthor>Author <div style={{color: "gray", padding: "1em 0", fontSize: "0.5rem"}}>2023.05.01</div></CommentAuthor>
-        <Comment>This is a comment...</Comment>
+        <CommentAuthor>{Postdata.Comments[0].User.nickname} 
+          <div style={{color: "gray", padding: "1em 0", fontSize: "0.5rem"}}>
+            {new Date(Postdata.Comments[0].createdAt).toString().substring(0, 21)} · 
+            <span style={{color: "blue"}}>수정</span> · 
+            <span style={{color: "red"}}>삭제</span>
+          </div>
+        </CommentAuthor>
+        <Comment>{Postdata.Comments[0].content}</Comment>
       </div>
     </>
   )
 }
+
+      {/* <MorePosts>
+        <MorePost>Prev Post <MorePostTitle>Prev title blah blah</MorePostTitle></MorePost>
+        <MorePost>Next Post <MorePostTitle>Next title blah blah</MorePostTitle></MorePost>
+      </MorePosts> */}
