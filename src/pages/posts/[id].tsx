aa239396c57aa15314108a63e3d22cc31b3f065c
ys9494 <mdlife94@gmail.com>
2023-05-18 12:44:20 +0900

    feat:post router

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import axios from 'axios';
 import * as api from '@/utils/api';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import auth from '@/components/common/auth';
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router';
 
 const Title = styled.div`
   width: 40%;
@@ -31,7 +31,7 @@ const CommentInputForm = styled.form`
   margin: 1em auto;
 `;
 
-const CommentInput = styled.textarea `
+const CommentInput = styled.textarea`
   width: 100%;
   height: 6em;
   background-color: #f1f3f5;
@@ -57,30 +57,30 @@ const UpdateComment = styled.input`
 `;
 
 interface CommentFormValue {
-  content: string
+  content: string;
 }
 
 interface PostDetail {
-  Category: string,
-  CategoryId: number,
-  Comments: 
-    { User: { nickname: string },
-    content: string,
-    createdAt: Date,
-    id: number }[]
-  ,
-  User: { nickname: string },
-  UserId: string,
-  content: string,
-  createdAt: Date,
-  deletedAt: Date,
-  id: number,
-  summary: string,
-  title: string,
-  updatedAt: Date,
-  userId: string,
-  views: number
-};
+  Category: string;
+  CategoryId: number;
+  Comments: {
+    User: { nickname: string };
+    content: string;
+    createdAt: Date;
+    id: number;
+  }[];
+  User: { nickname: string };
+  UserId: string;
+  content: string;
+  createdAt: Date;
+  deletedAt: Date;
+  id: number;
+  summary: string;
+  title: string;
+  updatedAt: Date;
+  userId: string;
+  views: number;
+}
 
 export default function Posts() {
   const {
@@ -91,36 +91,41 @@ export default function Posts() {
 
   const user = auth.currentUser || null;
   const router = useRouter();
-  const [Id, setId] = useState<string>("")
-  const [Postdata, setPostdata] = useState<PostDetail>({})
+  const [Id, setId] = useState<string>('');
+  const [Postdata, setPostdata] = useState<PostDetail>({});
 
   useEffect(() => {
-    setId(String(router.query.id))
-    getPost()
-    console.log(user)
-  }, []);
+    if (router.isReady) {
+      const id = String(router.query.id);
+      setId(id);
+      getPost(id);
+      console.log(user);
+    }
+  }, [router]);
 
-  const getPost = async () => {
-    const response = await axios.get(`http://3.39.79.138/api/v1/posts/1`);
-    console.log(response.data.data)
-    setPostdata(response.data.data)
+  const getPost = async (id: string) => {
+    const response = await api.get(`/posts/${id}`);
+    console.log(response.data?.data);
+    setPostdata(response.data?.data);
   };
 
-  const onCommentSubmitHandler: SubmitHandler<CommentFormValue> = async (data) => {
+  const onCommentSubmitHandler: SubmitHandler<
+    CommentFormValue
+  > = async data => {
     try {
-        await api.post(`/comments/${Id}`, data);
-        // window.location.replace("/msg")
-    } catch (error : any) {
-        alert(error);
+      await api.post(`/comments/${Id}`, data);
+      // window.location.replace("/msg")
+    } catch (error: any) {
+      alert(error);
     }
   };
 
-  const deleteComment = async (commentId: number)=>{
+  const deleteComment = async (commentId: number) => {
     try {
-        await api.delete(`/comments/${commentId}`);
-        // window.location.replace("/msg")
-    } catch (error : any) {
-        alert(error);
+      await api.delete(`/comments/${commentId}`);
+      // window.location.replace("/msg")
+    } catch (error: any) {
+      alert(error);
     }
   };
 
@@ -133,50 +138,63 @@ export default function Posts() {
   //   }
   // };
 
-
   return (
     <>
       <Title>{Postdata.title}</Title>
-      <Author>{Postdata.User.nickname} · <span style={{color: "gray", fontSize: "0.7rem"}}>{new Date(Postdata.createdAt).toString().substring(0, 21)}</span></Author>
+      <Author>
+        {Postdata.User?.nickname} ·{' '}
+        <span style={{ color: 'gray', fontSize: '0.7rem' }}>
+          {new Date(Postdata.createdAt).toString().substring(0, 21)}
+        </span>
+      </Author>
       <Post>{Postdata.content}</Post>
 
       <CommentInputForm onSubmit={handleSubmit(onCommentSubmitHandler)}>
-        <div style={{padding: "1em 0", fontSize: "0.7rem"}}>{Postdata.Comments.length} comments</div>
-        <CommentInput {...register('content', {
-          required: true,
-          minLength: 1,
-          maxLength: 200,
-        })} />
+        <div style={{ padding: '1em 0', fontSize: '0.7rem' }}>
+          {Postdata.Comments?.length} comments
+        </div>
+        <CommentInput
+          {...register('content', {
+            required: true,
+            minLength: 1,
+            maxLength: 200,
+          })}
+        />
         <button>Send</button>
       </CommentInputForm>
 
-      {
-        Postdata.Comments.map((comment) => {
-            return(
-              <div>
-                <CommentAuthor>{comment.User.nickname} 
-                  <div style={{color: "gray", padding: "1em 0", fontSize: "0.5rem"}}>
-                    {new Date(comment.createdAt).toString().substring(0, 21)}
-                    {
-                    user.displayName == comment.User.nickname ?
-                    <>
-                      <span style={{color: "blue"}}> · 수정 · </span>
-                      <span onClick={() => deleteComment(comment.id)} style={{color: "red"}}>삭제</span>
-                    </> : null
-                    }
-                  </div>
-                </CommentAuthor>
-                {
-                user.displayName == comment.User.nickname ?
-                <Comment>
-                  <UpdateComment placeholder={comment.content}></UpdateComment>
-                </Comment> :
-                <Comment>{comment.content}</Comment>
-                }
-              </div>                           
-            )
-        })
-      }
+      {Postdata.Comments?.map(comment => {
+        return (
+          <div key={comment.id}>
+            <CommentAuthor>
+              {comment.User.nickname}
+              <div
+                style={{ color: 'gray', padding: '1em 0', fontSize: '0.5rem' }}
+              >
+                {new Date(comment.createdAt).toString().substring(0, 21)}
+                {user?.displayName == comment.User.nickname ? (
+                  <>
+                    <span style={{ color: 'blue' }}> · 수정 · </span>
+                    <span
+                      onClick={() => deleteComment(comment.id)}
+                      style={{ color: 'red' }}
+                    >
+                      삭제
+                    </span>
+                  </>
+                ) : null}
+              </div>
+            </CommentAuthor>
+            {user?.displayName == comment.User.nickname ? (
+              <Comment>
+                <UpdateComment placeholder={comment.content}></UpdateComment>
+              </Comment>
+            ) : (
+              <Comment>{comment.content}</Comment>
+            )}
+          </div>
+        );
+      })}
     </>
-  )
+  );
 }
